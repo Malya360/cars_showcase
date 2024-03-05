2b8278c83f1e9ac722673cc9ae537620c823aedc
malya360 <abdulrahimymalya@gmail.com>
2024-03-06 02:00:57 +0300

    changes two

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import  CustomButton  from './CustomButton'
 
 type Props = {}
 
 const Hero = (props: Props) => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+    <div className='flex-1 pt-36 padding-x'>
+       <h1 className='hero__title'> 
+       Find, book, or rent a car - quickly and easily!
+       </h1> 
+       <p className='hero__subtitle'>
+        Streamline your car rental experience with our effortless booking process.  </p>
+
+        <CustomButton />
+        
+    </div>
+    </div>
   )
 }
 
