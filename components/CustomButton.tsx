838b993e5cd1de412da62e5ba2a1243115b44867
malya360 <abdulrahimymalya@gmail.com>
2024-03-07 00:20:36 +0300

    prog 2

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+"use client";
 
-const CustomButton = () => {
+import { CustomButtonProps } from "@/types";
+import Image  from "next/image";
+
+const CustomButton = ({title,containerStyles,handleClick}:CustomButtonProps) => {
   return (
-    <div>CustomButton</div>
+    <button
+        disabled={false}
+        type={"button"}
+        className={`custom-btn ${containerStyles}`}
+        onClick={handleClick}
+    >
+        <span className={`flex-1`}>
+           {title}
+        </span>
+
+    </button>
   )
 }
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,13 @@
+"use client";
 import React from 'react'
 import  CustomButton  from './CustomButton'
+import Image from 'next/image';
 
-type Props = {}
 
-const Hero = (props: Props) => {
+const Hero = () => {
+    const handleScroll=()=>{
+
+    }
   return (
     <div className='hero'>
     <div className='flex-1 pt-36 padding-x'>
@@ -13,8 +17,21 @@ const Hero = (props: Props) => {
        <p className='hero__subtitle'>
         Streamline your car rental experience with our effortless booking process.  </p>
 
-        <CustomButton />
-        
+        <CustomButton 
+         title= "Explore Cars"
+         containerStyles="bg-primary-blue text-white rounded-full mt-10"
+         handleClick={handleScroll}
+        />
+    </div>
+
+    <div className='hero__image-container'>
+       <div className='hero__image'>
+          <Image src="/hero.png"  alt="hero" fill className="object-contain"/>
+          
+          <div className='hero__image-overlay'>
+
+          </div>
+       </div>
     </div>
     </div>
   )
