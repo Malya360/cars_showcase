3f688761beb004824f9f0ecc9bb3f8efdd7c3ca0
malya360 <abdulrahimymalya@gmail.com>
2024-03-24 22:13:13 +0300

    reload

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -38,7 +38,7 @@ const CarCard = ({car}:CarCardProps) => {
         <div className='relative w-full h-40 my-3 object-contain'>
             <Image src={generateCarImageUrl(car)}  alt='car model' fill priority className='object-contain'/>
         </div>
-        
+    
         <div className='relative flex w-full mt-2'>
              <div className='flex group-hover:invisible w-full justify-between text-gray'>
 
